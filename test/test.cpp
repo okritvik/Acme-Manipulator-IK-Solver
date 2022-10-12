ba0a4ed8a4babb7b0f5bff8445026b7b74eabcae
okritvik <okritvik@gmail.com>
2022-10-11 21:30:53 -0400

    Added initial test cases

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,26 @@
 #include <gtest/gtest.h>
+#include <vector>
+#include "../include/Robot.hpp"
+Robot acme_kuka;
+std::vector<double> init_pose = {1, 2, 3, 4, 5, 6};
+std::vector<double> fin_pose = {1, 2, 3, 4, 5, 6};
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+TEST(Robot, should_pass1) {
+  EXPECT_EQ(acme_kuka.set_initial_pose(init_pose), true);
+}
+
+TEST(Robot, should_pass2) {
+  EXPECT_EQ(acme_kuka.set_final_pose(fin_pose), true);
+}
+
+TEST(Robot, should_pass3) {
+  EXPECT_EQ(acme_kuka.set_joint_angles(init_pose), true);
+}
+
+TEST(Robot, should_pass4) {
+  EXPECT_EQ(acme_kuka.get_joint_angles(init_pose), true);
+}
+
+TEST(Robot, should_pass5) {
+  EXPECT_EQ(acme_kuka.execute_path(), true);
 }
